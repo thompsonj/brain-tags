added parallel processing, fixed sorting in wrong order in validate(), added plotting and param learning functions

brain_tags.py
@@ -12,19 +12,24 @@ Algorithm adapted from Weston 2011
 
 # these are all external code not written by me
 import os, sys
-import random
 import numpy as np
 import bregman 
 from scipy.spatial.distance import pdist
 from scipy.spatial.distance import squareform
 from scipy.stats import ttest_ind
 from scipy.stats import ttest_rel
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+#from multiprocessing import Process, Queue
+import pprocess
+from pylab import *
+import random
+import time
 
 
-def WARP_opt_weighted3(Xs_train,Ys_train,k,C,D,gamma=.2,Xs_test=None,Ys_test=None,stop_pre=None):
+def WARP_opt_weighted3(stim_ind, Xs_train,Ys_train,k,C,D,learning_rate=.2,Xs_test=None,Ys_test=None,stop_pre=None):
     """
     Input:
+        stim_ind - unused, 1 based
         Xs - images
         Ys - annotations
         k - for evaluating precision@k
@@ -42,7 +47,7 @@ def WARP_opt_weighted3(Xs_train,Ys_train,k,C,D,gamma=.2,Xs_test=None,Ys_test=Non
         the tag prediction task.
 
     """
-    print 'learning rate:', gamma
+    print 'learning rate:', learning_rate
     n = Xs_train.shape[1]
     Y = Ys_train.shape[0] # size of dictionary
     d = Xs_train.shape[0]# dimensionality of images
@@ -109,12 +114,12 @@ def WARP_opt_weighted3(Xs_train,Ys_train,k,C,D,gamma=.2,Xs_test=None,Ys_test=Non
             gradient_W = np.zeros(W.shape)
             gradient_W[:,j] = L(int(np.floor((Y-1)/N)))*np.transpose(-V*x)
             gradient_W[:,y_hat_ind] = L(np.floor((Y-1)/N))*np.transpose(V*x)
-            W = W - (gamma*gradient_W)
+            W = W - (learning_rate*gradient_W)
             gradient_V = L(int(np.floor((Y-1)/N)))*(W[:,y_hat_ind]*np.transpose(x) - (W[:,j]*np.transpose(x))) 
-            V = V - (gamma*gradient_V)
+            V = V - (learning_rate*gradient_V)
         if not(c%100):
             if select_model:
-                print 'evaluating...'
+                #print 'evaluating...'
                 pre_tr,dist_tr = validate(W, V, Xs_train, Ys_train,k)
                 #pre_train = np.append(pre_train,pre_tr)
                 pre_train = np.append(pre_train,pre_tr)
@@ -136,15 +141,17 @@ def WARP_opt_weighted3(Xs_train,Ys_train,k,C,D,gamma=.2,Xs_test=None,Ys_test=Non
                 W_best = W
                 V_best = V
             print 'training precision',pre_train[-1]
-    return W_best,V_best,pre_train,pre_test,dist_train, dist_test,stop_train_pre,best_test_dist
+    return (stim_ind,W_best,V_best,pre_train,pre_test,dist_train, dist_test,stop_train_pre,best_test_dist)
 
-def plot_error_curves(dist_train, dist_test):
-
-    fig1 = plt.figure()
-    x= range(len(dist_train))
+def plot_error_curves(train, test, title='Euc Distance'):
+    #fig1 = plt.figure()
+    x= range(len(train))
     for i in x:
-        plt.plot(x, dist_train[str(i)])
-        plt.plot(x, dist_test[str(i)])
+        plot(x, train[str(i+1)],'b')
+        plot(x, test[str(i+1)],'r-')
+    legend(('training dist'), ('testing dist'))
+    title(title)
+    show()
 
 def WARP_opt(Xs, Ys,k,C,D):
     """
@@ -276,12 +283,11 @@ def WARP_opt(Xs, Ys,k,C,D):
         #print 'f: ', f_new
     return W,V
 
-
-"""
-Use learned mapping matrices W and V to the rank of all tags for each image
-Ys = 100x600
-"""
 def validate(W, V, Xs, Ys, k):
+    """
+    Use learned mapping matrices W and V to the rank of all tags for each image
+    Ys = 100x600
+    """
     # print 'in validate'
     ranks = np.transpose(W)*V*Xs
     # each column in rank is for a different image
@@ -323,6 +329,45 @@ def L(k):
         alpha = 1/j
         loss = loss + alpha
     return loss
+
+def learn_25_models(subject, k=5,C=1,D=50,learning_rate=.2,feat_expr='_sts+hg_600',tag_feat_file='weighted_top100_lastfm+pandora_feats.txt'):
+    t = time.time()
+    stim_labels = np.genfromtxt('stimuli_labels.txt')
+    brains = np.matrix(np.transpose(bregman.audiodb.adb.read('brain_features/'+subject+feat_expr+'.brain')))
+    n=brains.shape[1]
+    uni_stim_labels = np.unique(stim_labels)
+    stim_labels = np.ravel([np.tile(i,(1,3)) for i in stim_labels])
+    words = np.genfromtxt(tag_feat_file)
+    words = np.transpose(np.matrix([words[i-1,:]for i in stim_labels ])) # 600 # Y
+    Ws = {}
+    Vs = {}
+    all_dist_test = {}
+    all_dist_train = {}
+    all_best_test_dists = np.zeros(25)
+    queue = pprocess.Queue(limit=100)
+    WARP = queue.manage(pprocess.MakeParallel(WARP_opt_weighted3))
+    for stim in uni_stim_labels: # 1 based 
+        print stim
+        trainwords = words[:,stim_labels!=stim]
+        trainbrains = brains[:,stim_labels!=stim]
+        testwords = words[:,stim_labels==stim]
+        testbrains = brains[:,stim_labels==stim]
+        WARP(stim, trainbrains,trainwords,k,C,D,learning_rate,Xs_test=testbrains,Ys_test=testwords)
+    #time.sleep(20)
+    print 'Finishing...'
+    for i, W_best, V_best, pre_train ,pre_test, dist_train, dist_test, stop_train_pre, best_test_dist in queue:
+        print 'queue', i
+        Ws[str(i)] = W_best
+        Vs[str(i)] = V_best
+        all_dist_test[str(i)] = dist_test
+        all_dist_train[str(i)] = dist_train
+        all_pre_test[str(i)] = pre_test
+        all_pre_train[str(i)] = pre_train
+        all_best_test_dists[i-1] = best_test_dist
+    total_time = time.time() - t
+    print "Time taken:", total_time
+    avg_best_dist = np.mean(all_best_test_dists)
+    return Ws, Vs,all_dist_test,all_dist_train,all_pre_test,all_pre_train,total_time,avg_best_dist
 
 def evaluate_tag_prediction(subject,k=5,C=1,D=50,tag_feat_file='weighted_top100_lastfm+pandora_feats.txt'):
     # load brain data
@@ -378,33 +423,36 @@ def evaluate_brain_prediction3(subject='1mar11sj',stop_pre=.88,k=5,C=1,D=75,feat
     dist = squareform(pdist(np.transpose(words)))
     WARP_dist = np.zeros(n)
     KNN_dist = np.zeros(n)
-    Ws = {}
-    Vs = {}
-    all_dist_test = {}
-    all_dist_train = {}
-    trained = 0
-    # iterate through each 
+    
+    #trained = 0
+    # train models, one for each of the 25 stimuli
+    Ws,Vs,all_dist_test,all_dist_train,all_pre_test,all_pre_train,total_time,avg_best_dist = learn_25_models(subject)
+
+    # synthesize and evaluate predicted brain activity
+    # iterate through each data point
     for i in range(n):
         testtags = words[:,i]
-        print testtags.shape
         testbrain = brains[:,i]
         cur_stim = stim_labels[i]
-        if str(cur_stim) in Ws:
-            W_best = Ws[str(cur_stim)]
-            V_best = Vs[str(cur_stim)]
-        else:
-            trained+=1
-            print 'training ',trained, 'time'
-            trainwords = words[:,stim_labels!=stim_labels[i]]
-            trainbrains = brains[:,stim_labels!=stim_labels[i]]
-            testwords = words[:,stim_labels==stim_labels[i]]
-            testbrains = brains[:,stim_labels==stim_labels[i]]
-            #W_best,V_best,pre_train,pre_test,stop_train_pre,best_test_pre= WARP_opt_weighted3(trainbrains,trainwords,k,C,D,stop_pre=stop_pre)
-            W_best,V_best,pre_train,pre_test,dist_train, dist_test,stop_train_pre,best_test_dist= WARP_opt_weighted3(trainbrains,trainwords,k,C,D,Xs_test=testbrains,Ys_test=testwords)
-            Ws[str(cur_stim)] = W_best
-            Vs[str(cur_stim)] = V_best
-            all_dist_test[str(i)] = dist_test
-            all_dist_train[str(i)] = dist_train
+        W_best = Ws[str(cur_stim)]
+        V_best = Vs[str(cur_stim)]
+
+        # if str(cur_stim) in Ws:
+        #     W_best = Ws[str(cur_stim)]
+        #     V_best = Vs[str(cur_stim)]
+        # else:
+        #     trained+=1
+        #     print 'training ',trained, 'time'
+        #     trainwords = words[:,stim_labels!=stim_labels[i]]
+        #     trainbrains = brains[:,stim_labels!=stim_labels[i]]
+        #     testwords = words[:,stim_labels==stim_labels[i]]
+        #     testbrains = brains[:,stim_labels==stim_labels[i]]
+        #     #W_best,V_best,pre_train,pre_test,stop_train_pre,best_test_pre= WARP_opt_weighted3(trainbrains,trainwords,k,C,D,stop_pre=stop_pre)
+        #     W_best,V_best,pre_train,pre_test,dist_train, dist_test,stop_train_pre,best_test_dist= WARP_opt_weighted3(trainbrains,trainwords,k,C,D,Xs_test=testbrains,Ys_test=testwords)
+        #     Ws[str(cur_stim)] = W_best
+        #     Vs[str(cur_stim)] = V_best
+        #     all_dist_test[str(i)] = dist_test
+        #     all_dist_train[str(i)] = dist_train
         brainrank = np.array([(validate(W_best, V_best, np.transpose(brain), testtags, k)) for brain in np.transpose(trainbrains)])
         # indices of brains whose distance (after turning rank into prob) to actual tag vec is shortest
         top_brain_ind = np.argsort(brainrank[:,1])[:k]
@@ -422,7 +470,7 @@ def evaluate_brain_prediction3(subject='1mar11sj',stop_pre=.88,k=5,C=1,D=75,feat
         print 'WARP_dist:', WARP_dist
         KNN_dist[i]= np.linalg.norm(pred_brain_knn-testbrain)
         print 'KNN_dist:', KNN_dist
-    print 'Model trained only ',trained, 'times'
+    #print 'Model trained only ',trained, 'times'
     #t,prob = ttest_ind(WARP_dist,KNN_dist)
     t,prob = ttest_rel(WARP_dist,KNN_dist)
     print 't:',t,'prob:',prob
@@ -498,20 +546,25 @@ def knn_tags(k=5.0, ntags=5.0, tag_feat_file='weighted_top100_lastfm+pandora_fea
     avg_pre = np.mean(pre,1)
     return pre,avg_pre
 
-# def learn_params(subject, feat_expr='_sts+hg_600',tag_feat_file='weighted_top100_lastfm+pandora_feats.txt'):
+def learn_params(subject, feat_expr='_sts+hg_600',tag_feat_file='weighted_top100_lastfm+pandora_feats.txt'):
+    """
+    """
+    #subjects = ['1mar11sj','1mar11yw','5mar11ad','5mar11at','8mar11am','8mar11ec','9mar11ab','9mar11jd','16mar11hy','16mar11mg','16mar11mh','16mar11sg','17mar11sw','26feb11kj','26feb11zi']
+    #subjects = ['1mar11sj','1mar11yw']
+    #Ds = [25,35,50,75,100,200,600]
+    Ds=[50,100]
+    #learning_rates=[.01,.05, .1, .15 .2, .25 .3, .05, .06 ]
+    learning_rates=[.1, .2]
 
-#     Ds = [25,35,50,75,100,200,600]
-#     gammas[.01,.05, .1, .15 .2, .25 .3, .05, .06 ]
-#     for D in Ds:
-#         for gamma in gammas:
-#             # load brain data
-#             stim_labels = np.genfromtxt('stimuli_labels.txt')
-#             stim_labels = np.ravel([np.tile(i,(1,3)) for i in stim_labels])
-#             words100 = np.genfromtxt(tag_feat_file)
-#             words = np.transpose(np.matrix([words100[i-1,:]for i in stim_labels ]))
-#             brains = np.matrix(np.transpose(bregman.audiodb.adb.read('brain_features/'+subject+feat_expr+'.brain')))
-#             W_best,V_best,pre_train,pre_test,dist_train, dist_test,stop_train_pre,best_test_dist=WARP_opt_weighted3(Xs_train,Ys_train,k,C,D,gamma=gamma,Xs_test=None,Ys_test=None,stop_pre=None)
-
+    for D in Ds:
+        for lr in learning_rates:
+            # load brain data
+            stim_labels = np.genfromtxt('stimuli_labels.txt')
+            stim_labels = np.ravel([np.tile(i,(1,3)) for i in stim_labels])
+            words100 = np.genfromtxt(tag_feat_file)
+            words = np.transpose(np.matrix([words100[i-1,:]for i in stim_labels ]))
+            brains = np.matrix(np.transpose(bregman.audiodb.adb.read('brain_features/'+subject+feat_expr+'.brain')))
+            Ws, Vs,all_dist_test,all_dist_train,all_pre_test,all_pre_train,total_time,avg_best_dist=learn_25_models(subject)
 
 def main():
     #subjects = ['1mar11sj','1mar11yw','5mar11ad','5mar11at','8mar11am','8mar11ec','9mar11ab','9mar11jd','16mar11hy','16mar11mg','16mar11mh','16mar11sg','17mar11sw','26feb11kj','26feb11zi']

save_brain_feats.py
@@ -3,21 +3,150 @@ from scandora import *
 import scandora
 import bregman as br
 import pytagcrawl as T
+import numpy as np
+import itertools
 
-def save_brain_features():
+def save_brain_features(ev_data=False, avg=True):
+    
     subjects = ['1mar11sj','1mar11yw','5mar11ad','5mar11at','8mar11am','8mar11ec','9mar11ab','9mar11jd','16mar11hy','16mar11mg','16mar11mh','16mar11sg','17mar11sw','26feb11kj','26feb11zi']
     targets = '/global/data/casey/FMRI/pandora_dt_sm.labels.fixed'
     outpath = '/global/data/casey/thompson/brain_tags/brain_features/'
     for subject in subjects:
+        print subject
         niibrain = '/global/data/casey/FMRI/'+subject+'.pandora_dt_sm.nii'
-        mask = '/global/data/casey/FMRI/drawing/'+subject+'.resamp+sts+orig.nii.gz'
-        ds = load_fmri_data(niibrain, targets, ev_data=True, mask=mask)
+        mask = '/global/data/casey/FMRI/drawing/'+subject+'.resamp+sts+hg+orig.nii.gz'
+        ds = load_fmri_data(niibrain, targets, ev_data=ev_data, mask=mask)
         ds = ds[ds.sa.targets < 6]
-        Brain, Vb, iVb = svd_reduce_data(ds.S, 0.99)
-        bregman.audiodb.adb.write(outpath + subject + '_200_99.brain', Brain)
+        if avg:
+            # load stimuli labels
+        stim_labels = '/global/data/casey/FMRI/stimuli_labels.txt'
+        stimuli_ids = np.array([line.strip() for line in open(stim_labels)])
+        stimuli_ids_tr = list(itertools.chain(*[[sid+'a', sid+'b', sid+'c'] for sid in stimuli_ids]))
+            ds.sa['stim_ids'] = stimuli_ids_tr
+            ds = np.array([np.mean(ds[ds.sa.stim_ids==i],0) for i in np.unique(stimuli_ids_tr)])
+            fname = '_sts+hg_avgrun_75x72.brain'
+        else:
+            fname = '_sts+hg_600.brain'
+        Brain, Vb, iVb = svd_reduce_data(ds,.99)
+        bregman.audiodb.adb.write(outpath + subject + fname, Brain)
         
 def save_tags():
     A = T.TrackTags() 
-    A._run_track_top_tags()
+    A.tag_crawler()
+    #A._run_track_top_tags()
     A._run_tag_tag_intersection()
-    A.unweighted_top_tags_to_feature_vectors('new_top100_tags_features.txt',100)
+    #A.unweighted_top_tags_to_feature_vectors('new_top100_tags_features.txt',100)
+    A.weighted_top_tags_to_feature_vectors('weighted_top100_tags_features.txt',100)
+    lastfm_feats = A.track_features
+    pandora_feats = get_pandora_properties()
+
+
+def get_pandora_properties():
+    """
+    Iterate through pandora properties files to generate feature vectors for each track. 
+    Set all values in feature matrix to 0 or 100 to match probabilities of last.fm tag vectors.
+    Save pandora matrix to file.
+    Return pandora matrix to be concatenated to last.fm feature matrix.
+    """
+    unique_props = []
+    #n_unique = 113
+    #feature_mat = np.matrix()
+    propdir = '/global/data/casey/FMRI/PandoraData/Properties/'
+    for n,f in enumerate(TrackListA):
+        print '**'
+        print f
+        properties = [line.strip() for line in open(propdir+'/'+f)]
+        for m,p in enumerate(properties):
+            if p in unique_props:
+                i = unique_props.index(p)
+                feature_mat[n,i] = 100
+            else:
+                unique_props.append(p)
+                if n==0 and m==0:
+                    feature_mat = np.zeros((25,1))
+                    feature_mat[0,0] = 100
+                else:
+                    feature_mat = np.append(feature_mat, np.zeros((25,1)), 1)
+                    feature_mat[n,-1] = 100
+    # write features to file
+    fname = 'pandora_feats.txt'
+    if fname:
+        try:
+            np.savetxt(fname, feature_mat, fmt='%d')
+        except:
+            print "Could not save features to file %s\n" %(fname)
+            raise
+    return feature_mat
+
+def save_weighted_and_pandora():
+    """
+    min number of tags=10, thus can evaluate p@<=10
+    """
+    lastfm_prob_feats = np.genfromtxt('weighted_top100_tags_features.txt')
+    pandora_feats = get_pandora_properties()
+    allfeats=np.append(lastfm_prob_feats, pandora_feats, 1)
+    # write features to file
+    fname = 'weighted_top100_lastfm+pandora_feats.txt'
+    if fname:
+        try:
+            np.savetxt(fname, allfeats, fmt='%d')
+        except:
+            print "Could not save features to file %s\n" %(fname)
+            raise
+
+
+TrackListA=[
+    "Brian Eno/brian+eno_a+clearing",
+    "Brian Eno/various+artists+music+for+films+3_theme+from+creation",
+    "Brian Eno/eno+moebius+roedelius_old+land",
+    "Brian Eno/galerie+stratique_horizons+lointains",
+    "Brian Eno/anugama_io+moon+of+jupiter",
+    "Eddie Cochran/elvis+presley_jailhouse+rock",
+    "Eddie Cochran/bill+haley_shake+rattle+and+roll",
+    "Eddie Cochran/little+richard_bama+lama+bama+loo",
+    "Eddie Cochran/ritchie+valens_come+on+lets+go",
+    "Eddie Cochran/eddie+cochran_money+honey+live",
+    "Ozzy Osbourne/ozzy+osbourne_fire+in+the+sky",
+    "Ozzy Osbourne/judas+priest_youve+got+another+thing+comin",
+    "Ozzy Osbourne/metallica_of+wolf+and+man",
+    "Ozzy Osbourne/ac+dc_you+shook+me+all+night+long",
+    "Ozzy Osbourne/scorpions_rock+you+like+a+hurricane",
+    "Romantic/roger+norrington_sym+no9+in+d+op125+choral+2+molto+vivace+yvonne+kenny+sarah+walker+patrick+power+petteri+salomaa+schutz+chor+of+london_beethoven_symphony+number+9",
+    "Romantic/colorado+symphony+orchestra_finale+allegro+con+fuoco_tchaikovsky_symphony+number+4",
+    "Romantic/eugene+ormandy_3+vivacissimo_sibelius_symphony+number+2",
+    "Romantic/royal+philharmonic+orchestra+schubert-symphonies+nos+3+5+6+beecham+royal+philharmonic+orchestra",
+    "Romantic/roger+norrington_sym+no6+in+f+op68+pastoral+i+allegro+non+troppo+awakening+of+happy+feelings+on+arriving_beethoven_symphony+number+6",
+    "Waylon Jennings/waylon+jennings_are+you+sure+hank+done+it+this+way",
+    "Waylon Jennings/willie+nelson_me+and+paul",
+    "Waylon Jennings/merle+haggard_pancho+and+lefty",
+    "Waylon Jennings/hank+williams+junior_whiskey+bent+and+hell+bound",
+    "Waylon Jennings/willie+nelson+waylon+jennings+johnny+cash+kris+kristofferson_welfare+line",
+]
+
+TrackListB=[
+    ["Brian Eno", "Brian Eno", "A Clearing "],
+    ["Brian Eno", "Brian Eno", "Theme from 'Creation' "],
+    ["Brian Eno", "Eno, Moebius & Roedelius", "Old Land "],
+    ["Brian Eno", "Galerie Stratique", "Horizons Lointains "],
+    ["Brian Eno", "Anugama", "IO-Moon of Jupiter "],
+    ["Eddie Cochran", "Elvis Presley", "Jailhouse Rock "],
+    ["Eddie Cochran", "Bill Haley", "Shake Rattle and Roll "],
+    ["Eddie Cochran", "Little Richard", "Bama Lama Bama Loo "],
+    ["Eddie Cochran", "Ritchie Valens", "Come On Let's Go "],
+    ["Eddie Cochran", "Eddie Cochran", "Money Honey "],
+    ["Ozzy Osbourne", "Ozzy Osbourne", "Fire in the Sky "],
+    ["Ozzy Osbourne", "Judas Priest", "You've Got Another Thing Coming "],
+    ["Ozzy Osbourne", "Metallica", "Of Wolf & Man "],
+    ["Ozzy Osbourne", "ACDC", "You Shook Me All Night Long "],
+    ["Ozzy Osbourne", "Scorpions", "Rock You Like A Hurricane "],
+    ["Romantic Symphony", "Beethoven", "Symphony No. 9 Mvt. 2 "],
+    ["Romantic Symphony", "Tchaikovsky", "Symphony No. 4 Mvt. 4 "],
+    ["Romantic Symphony", "Sibelius", "Symphony No. 2 Mvt. 4 "],
+    ["Romantic Symphony", "Schubert", "Symphony No. 5 Mvt. 1 "],
+    ["Romantic Symphony", "Beethoven", "Symphony No. 6 Mvt. 1 "],
+    ["Waylon Jennings", "Waylon Jennings", "Are You Sure Hank Done It This Way "],
+    ["Waylon Jennings", "Willie Nelson", "Me and Paul "],
+    ["Waylon Jennings", "Merle Haggard", "Pancho and Lefty "],
+    ["Waylon Jennings", "Hank Williams Jr", "Whiskey Bent and Hell Bound "],
+    ["Waylon Jennings", "Willie Nelson", "Welfare Line "],
+]